ad93feb469abff41976c7490732e580a13617f01
MR-Mohammadi-sab <asf423796@gmail.com>
2024-11-20 23:46:51 -0800

    update

--- src/componets/outlet/OutLet.jsx
+++ src/componets/outlet/OutLet.jsx
@@ -4,7 +4,7 @@ import { Post } from './posts/post'
 
 export const OutLet = () => {
   return (
-    <div className='lg:ml-[18%] ml-[30%] pr-4 lg:pr-0 lg:max-w-[51%]  mt-12 overflow-hidden flex flex-col space-x-3'>
+    <div className='lg:ml-[18%] md:ml-[30%] pr-4 lg:pr-0 lg:max-w-[51%]  mt-12 overflow-hidden flex flex-col space-x-3'>
       <Stroes />
       <Post />
     </div>
